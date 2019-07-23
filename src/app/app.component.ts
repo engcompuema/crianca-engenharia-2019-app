2a0ba1b42d74eee76f062aebcd39d7e755aae6f2
Lucas <lucas.ferreira@seati.ma.gov.br>
2019-07-23 12:11:49 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crianca-engenharia-app';
+
+  collapse() {
+    var element = document.getElementById("sidebar") as HTMLElement;
+    const hasClass = element.classList.contains("active");
+    
+      if(hasClass){
+        element.classList.remove('active')
+      }else{
+        element.classList.add('active')
+      }
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { ConfirmDialogModule, GrowlModule, ConfirmationService } from 'primeng/primeng';
+import { TableModule } from 'primeng/table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,9 +14,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TableModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule,
+    GrowlModule,
+    ConfirmDialogModule,
   ],
-  providers: [],
+  providers: [ConfirmationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
